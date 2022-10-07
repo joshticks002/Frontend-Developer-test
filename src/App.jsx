a23399153c0d044681647c4765f98e8c6188f777
Legacy721 <jadeyemo002@gmail.com>
2022-10-07 08:16:21 +0100

    initialize app

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="App">This is the art gallery</div>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
+);
